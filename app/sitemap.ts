f5f0ac78251f423682dd718c2ec31f1ddb57246b
Arnab Chakraborty <iam-arnab@outlook.com>
2025-03-02 01:26:33 +0530

    setup 301 redirect

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: `${process.env.BASE_URL}`,
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 1.0,
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+      return [
+          {
+              source: '/:path*',
+              has: [
+                  {
+                      type: 'host',
+                      value: 'exiftuner.zytact.in',
+                  },
+              ],
+              destination: 'https://exiftuner.zytact.com/:path*',
+              permanent: true, // This sets up a 301 redirect
+          },
+      ];
+  },
+};
 
 export default nextConfig;
