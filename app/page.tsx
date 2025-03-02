dff749a12c2e5416d33a375ac6b0db1601ef1b35
Arnab Chakraborty <iam-arnab@outlook.com>
2025-03-02 12:04:17 +0530

    update link

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
                 <ImageForm />
                 <p className="mt-20">
                     Made with ❤️ by{' '}
-                    <Link href="https://zytact.in" className="font-bold">
+                    <Link href="https://zytact.com" className="font-bold">
                         Arnab
                     </Link>
                 </p>
